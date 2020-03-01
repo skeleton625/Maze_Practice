934239c0843f1d0dc5a54addbe048d043bdbb070
WaniFi <chaos0824@naver.com>
2020-03-01 22:15:34 +0900

    미로의 한 공간(space)을 담당하는 클래스 스크립트 구현

---
+++ Assets/Resources/Scripts/Cell.cs
@@ -0,0 +1,28 @@
+﻿
+public class Cell
+{
+    private int row, col;
+    public int Row
+    { get => row; }
+    public int Col
+    { get => col; }
+    private Cell Parent;
+
+    public Cell(int _row, int _col, Cell _parent)
+    {
+        this.row = _row;
+        this.col = _col;
+        this.Parent = _parent;
+    }
+
+    public Cell GetChild()
+    {
+        return new Cell((Row << 1) - Parent.Row, (Col << 1) - Parent.Col, null);
+    }
+
+    public override bool Equals(object obj)
+    {
+        Cell _obj = (Cell)obj;
+        return (Row.Equals(_obj.Row) && Col.Equals(_obj.Col));
+    }
+}
