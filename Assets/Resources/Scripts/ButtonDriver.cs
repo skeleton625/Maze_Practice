4ca2a213274dfc26320ca20158b90a5f7c620500
WaniFi <chaos0824@naver.com>
2020-03-02 21:17:33 +0900

    버튼 관리 스크립트 추가(Prim 알고리즘 버튼 추가)

---
+++ Assets/Resources/Scripts/ButtonDriver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDriver : MonoBehaviour
+{
+    private GeneratorDriver GD;
+
+    private void Start()
+    {
+        GD = GeneratorDriver.instance;
+    }
+
+    public void OnClickMazeAlgorithm(int _num)
+    {
+        GD.ChooseGeneratingMaze(_num);
+    }
+}
