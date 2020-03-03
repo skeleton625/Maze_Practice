2e54e085a05df940b060858f655a7135a2d07538
WaniFi <chaos0824@naver.com>
2020-03-03 23:28:09 +0900

    HuntAndKill 알고리즘을 위한 수정 및 복구 진행

--- Assets/Resources/Scripts/Maze.cs
+++ Assets/Resources/Scripts/Maze.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// '#' : -1, ' ' : 0, 'S' : 1, 'E' : 2, 'V' : 3
 public class Maze
 {
     public enum CELL_T{ WALL, FLOOR };
 
     private int Rows, Cols;
-    private Cell Start, End;
-    private char[,] Grid;
+    private int[,] Grid;
     private int[,] offset;
 
     public Maze(int _row, int _col)
@@ -16,16 +16,15 @@ public class Maze
         // 행, 열 길이를 홀수로 정의
         Rows = (_row % 2) == 1 ? _row : _row - 1;
         Cols = (_col % 2) == 1 ? _col : _col - 1;
-        Grid = new char[Rows, Cols];
+        Grid = new int[Rows, Cols];
 
         // 2차원 배열(리스트)를 모두 벽 Type으로 초기화
         for(int i = 0; i < Rows; i++)
         {
             for (int j = 0; j < Cols; j++)
-                Grid[i, j] = '#';
+                Grid[i, j] = -1;
         }
 
-        Start = End = null;
         // 상하좌우 배열 이동 값 초기화
         offset = new int[4, 2] { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
     }
@@ -42,9 +41,8 @@ public class Maze
             _nrow = _cell.Row + offset[i, 0];
             _ncol = _cell.Col + offset[i, 1];
 
-            bool validFloor = (_type.Equals(CELL_T.FLOOR) && IsPathable(_nrow, _ncol));
             bool validWall = (_type.Equals(CELL_T.WALL) && IsWall(_nrow, _ncol));
-            if (validFloor || validWall)
+            if (validWall)
                 _neighbors.Add(new Cell(_nrow, _ncol, _cell));
         }
 
@@ -52,19 +50,14 @@ public class Maze
         return _neighbors;
     }
 
-    public char[,] GetMazeMap()
+    public int[,] GetMazeMap()
     {
         return Grid;
     }
 
-    public ref char At(int r, int c)
+    public ref int At(int r, int c)
     {
         return ref Grid[r, c];
-    }
-
-    public bool IsPathable(int r, int c)
-    {
-        return (IsValid(r, c) && !(Grid[r, c].Equals('#') || Grid[r, c].Equals('.')));
     }
 
     public bool IsValid(int r, int c)
@@ -74,6 +67,6 @@ public class Maze
 
     public bool IsWall(int r, int c)
     {
-        return IsValid(r, c) && Grid[r, c].Equals('#');
+        return IsValid(r, c) && Grid[r, c].Equals(-1);
     }
 }
