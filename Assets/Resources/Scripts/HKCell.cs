1762dc8af8fc7c4356b605f629a1628f481cb47c
WaniFi <chaos0824@naver.com>
2020-03-04 21:06:59 +0900

    HunandKill 알고리즘 용 cell 클래스 구현

---
+++ Assets/Resources/Scripts/HKCell.cs
@@ -0,0 +1,23 @@
+﻿public class HKCell : Cell
+{
+    private bool visited;
+    public bool Visited
+    {
+        get => visited;
+        set => visited = value;
+    }
+    private bool[] sideWall;
+    public bool this[int index]
+    {
+        get => sideWall[index];
+        set => sideWall[index] = value;
+    }
+
+    public HKCell(int _row, int _col)
+    {
+        this.row = _row;
+        this.col = _col;
+        this.visited = false;
+        this.sideWall = new bool[4] { true, true, true, true};
+    }
+}
