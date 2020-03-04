840000d6ccb04f69ea9167c92d437378bd7636e5
WaniFi <chaos0824@naver.com>
2020-03-04 21:11:46 +0900

    HuntandKill 알고리즘 Maze 클래스 구현

---
+++ Assets/Resources/Scripts/HKMaze.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HKMaze : Maze<HKCell, HKCell>
+{
+    public HKMaze(int _row, int _col, HKCell _init)
+        : base(_row, _col, _init) { }
+
+    public override List<HKCell> GetNeighbors(HKCell _cell, bool _wall)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool IsMoveable(int r, int c)
+    {
+        int _nr, _nc;
+        for(int i = 0; i < 4; i++)
+        {
+            _nr = r + offset[i, 0];
+            _nc = c + offset[i, 1];
+            if (IsValid(_nr, _nc) && !Grid[_nr, _nc].Visited)
+                return true;
+        }
+        return false;
+    }
+}
