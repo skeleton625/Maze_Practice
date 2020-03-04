3cc83d97386c7b905b47689dc034933145fc648c
WaniFi <chaos0824@naver.com>
2020-03-04 21:07:19 +0900

    Prim 알고리즘 용 Cell 클래스 구현

---
+++ Assets/Resources/Scripts/PrimCell.cs
@@ -0,0 +1,23 @@
+﻿
+public class PrimCell : Cell
+{
+    private PrimCell Parent;
+
+    public PrimCell(int _row, int _col, PrimCell _parent)
+    {
+        this.row = _row;
+        this.col = _col;
+        this.Parent = _parent;
+    }
+
+    public PrimCell GetChild()
+    {
+        return new PrimCell((Row << 1) - Parent.Row, (Col << 1) - Parent.Col, null);
+    }
+
+    public override bool Equals(object obj)
+    {
+        PrimCell _obj = (PrimCell)obj;
+        return (Row.Equals(_obj.Row) && Col.Equals(_obj.Col));
+    }
+}
