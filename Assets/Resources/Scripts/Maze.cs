d479caae9cc5aca9e47bfbfc4a626751dd3606bd
WaniFi <chaos0824@naver.com>
2020-03-05 17:45:47 +0900

    Maze 생성자 내에서 배열 Grid 초기화 과정을 제거

--- Assets/Resources/Scripts/Maze.cs
+++ Assets/Resources/Scripts/Maze.cs
@@ -5,20 +5,13 @@ public abstract class Maze<T, U>
     protected int Rows, Cols;
     protected T[,] Grid;
 
-    public Maze(int _row, int _col, T _init)
+    public Maze(int _row, int _col)
     {
         Rows = (_row % 2) == 1 ? _row : _row - 1;
         Cols = (_col % 2) == 1 ? _col : _col - 1;
         // 행, 열 길이를 홀수로 정의
         Grid = new T[Rows, Cols];
         offset = new int[4, 2] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-
-        // 2차원 배열(리스트)를 모두 벽 Type으로 초기화
-        for (int i = 0; i < Rows; i++)
-        {
-            for (int j = 0; j < Cols; j++)
-                Grid[i, j] = _init;
-        }
     }
 
     public bool IsValid(int r, int c)
