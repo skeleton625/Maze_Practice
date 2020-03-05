43f61786cb52bad817b9c6e1c8872f5f2ea6d221
WaniFi <chaos0824@naver.com>
2020-03-05 17:43:18 +0900

    SideWall의 index 설정 기능 수정

--- Assets/Resources/Scripts/HuntandKill/HKCell.cs
+++ Assets/Resources/Scripts/HuntandKill/HKCell.cs
@@ -7,10 +7,30 @@
         set => visited = value;
     }
     private bool[] sideWall;
+    public bool[] SidelWall
+    { get => sideWall; }
     public bool this[int index]
     {
         get => sideWall[index];
-        set => sideWall[index] = value;
+        set
+        {
+            switch(index)
+            {
+                case 4:
+                    index = 1;
+                    break;
+                case 5:
+                    index = 0;
+                    break;
+                case 6:
+                    index = 3;
+                    break;
+                case 7:
+                    index = 2;
+                    break;
+            }
+            sideWall[index] = value;
+        }
     }
 
     public HKCell(int _row, int _col)
