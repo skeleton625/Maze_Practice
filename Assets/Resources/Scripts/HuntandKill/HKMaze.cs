5b2f87419722160d24d97cc14abc3b21d3745f21
WaniFi <chaos0824@naver.com>
2020-03-05 17:45:36 +0900

    DestroyAdjacentWall 함수 구현 및 생성자 내 Grid 초기화 코드 추가

--- Assets/Resources/Scripts/HuntandKill/HKMaze.cs
+++ Assets/Resources/Scripts/HuntandKill/HKMaze.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class HKMaze : Maze<HKCell, HKCell>
 {
-    public HKMaze(int _row, int _col, HKCell _init)
-        : base(_row, _col, _init) { }
+    public HKMaze(int _row, int _col)
+        : base(_row, _col)
+    {
+        for(int i = 0; i < Rows; i++)
+        {
+            for(int j = 0; j < Cols; j++)
+                Grid[i, j] = new HKCell(i, j);
+        }
+    }
 
     public override List<HKCell> GetNeighbors(HKCell _cell, bool _wall)
     {
@@ -23,5 +30,21 @@ public class HKMaze : Maze<HKCell, HKCell>
                 return true;
         }
         return false;
+    }
+
+    public void DestroyAdjacentWall(int r, int c)
+    {
+        int _nr, _nc;
+        for(int i = 0; i < 4; i ++)
+        {
+            _nr = r + offset[i, 0];
+            _nc = c + offset[i, 1];
+            if(IsValid(_nr, _nc) && Grid[_nr, _nc].Visited)
+            {
+                Grid[r, c][i] = false;
+                Grid[_nr, _nc][4 + i] = false;
+                break;
+            }
+        }
     }
 }
