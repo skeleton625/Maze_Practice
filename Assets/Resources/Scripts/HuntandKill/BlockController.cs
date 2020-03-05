2706b8fd645457efbbc4ad210f9aa58b7cd436cb
WaniFi <chaos0824@naver.com>
2020-03-05 17:47:11 +0900

    SlimWall 관리 스크립트 추가

---
+++ Assets/Resources/Scripts/HuntandKill/BlockController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Blocks;
+
+    public void SetBlockEnable(bool[] _block)
+    {
+        for(int i = 0; i < 4; i++)
+            Blocks[i].SetActive(_block[i]);
+    }
+}
