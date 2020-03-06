c76cd0757fa41b0bffeb2b66c0e6c544d870f15a
WaniFi <chaos0824@naver.com>
2020-03-06 23:24:46 +0900

    Scene 내 UI를 관리하는 스크립트 추가

---
+++ Assets/Resources/Scripts/UIController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MenuUI;
+    [SerializeField]
+    private InputField Row, Column;
+
+    public static UIController instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public int GetInputFieldRow()
+    {
+        int _row;
+        if (int.TryParse(Row.text, out _row))
+            return _row;
+        else
+            return 0;
+    }
+
+    public int GetInputFieldCol()
+    {
+        int _col;
+        if (int.TryParse(Row.text, out _col))
+            return _col;
+        else
+            return 0;
+    }
+}
