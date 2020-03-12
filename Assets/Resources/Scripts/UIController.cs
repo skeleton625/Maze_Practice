ee08030289941db67262ef29eb3df273c9b61e02
WaniFi <chaos0824@naver.com>
2020-03-12 22:45:20 +0900

    Player의 Object가 FinishPoint Object에 도달 시, Finish UI가 화면에 표현되도록 수정

--- Assets/Resources/Scripts/UIController.cs
+++ Assets/Resources/Scripts/UIController.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject MenuUI;
+    private GameObject MenuUI, FinishUI;
     [SerializeField]
     private GameObject TimerUI;
     [SerializeField]
     private InputField Row, Column;
     [SerializeField]
     private Text Minute, Seconds;
+    [SerializeField]
+    private Text FinMin, FinSec;
+    [SerializeField]
+    private FinishCollider FinishPoint;
 
     public static UIController instance;
     private void Awake()
@@ -42,6 +46,8 @@ public class UIController : MonoBehaviour
         int min = 0;
         double sec = 0;
 
+        FinishPoint = GameObject.Find("FinishPoint").GetComponent<FinishCollider>();
+
         MenuUI.SetActive(false);
         TimerUI.SetActive(true);
 
@@ -54,8 +60,16 @@ public class UIController : MonoBehaviour
                 ++min;
                 Minute.text = min.ToString();
             }
+
+            if (FinishPoint.IsFinish)
+                break;
+
             Seconds.text = string.Format("{0:0}", sec);
             yield return null;
         }
+
+        FinMin.text = Minute.text;
+        FinSec.text = Seconds.text;
+        FinishUI.SetActive(true);
     }
 }
