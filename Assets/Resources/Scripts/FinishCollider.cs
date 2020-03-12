4fafcea40fc888ab437da8dd55a083f45039d788
WaniFi <chaos0824@naver.com>
2020-03-12 22:45:53 +0900

    FinishPoint Object의 Trigger 충돌을 관리하는 스크립트 추가

---
+++ Assets/Resources/Scripts/FinishCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishCollider : MonoBehaviour
+{
+    private bool isFinish;
+    public bool IsFinish
+    { get => isFinish; }
+
+    private void Awake()
+    {
+        isFinish = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+            isFinish = true;
+    }
+}
