41517a59166edea20bcd9d6dbe80a30ccbc9d8f1
WaniFi <chaos0824@naver.com>
2020-11-29 23:27:47 +0900

    크루스칼 알고리즘 미로 생성 클래스 정의 중

---
+++ Assets/Resources/Scripts/Kruskals/KrusCell.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+public class KrusCell : Cell
+{
+    public KrusCell RightCell = null;
+    public KrusCell LowerCell = null;
+
+    public KrusCell(int r, int c)
+    {
+        row = r;
+        col = c;
+    }
+}
---
+++ Assets/Resources/Scripts/Kruskals/KrusKalMaze.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KruskalMaze : Maze<KrusCell, KrusCell>
+{
+    public KruskalMaze(int _row, int _col)
+    : base(_row, _col)
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Cols; j++)
+            {
+                Grid[i, j] = new KrusCell(i, j);
+                if (IsValid(i, j + 1)) Grid[i, j].RightCell = Grid[i, j + 1];
+                if (IsValid(i + 1, j)) Grid[i, j].LowerCell = Grid[i + 1, j];
+            }
+        }
+    }
+
+    public override bool IsMoveable(int r, int c)
+    {
+        throw new System.NotImplementedException();
+    }
+}
